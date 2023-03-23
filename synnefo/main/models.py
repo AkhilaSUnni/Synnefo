6b34556922f6e1315c373256aa641580dc68fa74
AkhilaSUnni <119470363+AkhilaSUnni@users.noreply.github.com>
2023-03-23 09:55:36 +0530

    Add files via upload

---
+++ synnefo/main/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+    Dept_name=models.CharField(max_length=100)
+    def __str__(self) :
+        return self.Dept_name
+class Batch(models.Model):
+    Batch_time=models.CharField(max_length=100)
+    def __str__(self) :
+        return self.Batch_time
+class  Student(models.Model):
+    Std_name=models.CharField(max_length=100)
+    Address=models.CharField(max_length=300)
+    Dept=models.ForeignKey(Department,default=1,on_delete=models.CASCADE)
